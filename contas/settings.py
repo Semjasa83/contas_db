c67d9cc524b11bf17bc993687f5ddebfabbbc426
Semjasa <Marceldezanna@yahoo.de>
2024-11-15 20:24:57 +0100

    bugfix on urls and settings

--- contas/settings.py
+++ contas/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'contas_db',
+    'contas'
 ]
 
 MIDDLEWARE = [
@@ -49,8 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'rest_framework',
-    'contas_app'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
--- contas_db/api/urls.py
+++ contas_db/api/urls.py
@@ -1,6 +1,9 @@
-from django.db import router
 from django.urls import include, path
 from .views import ContactViewSet
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'contacts', ContactViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
