f2ed8d7d12228613d60f065701c414b12bb2447b
Semjasa <Marceldezanna@yahoo.de>
2025-01-13 12:26:07 +0100

    model with company

--- contas_db/models.py
+++ contas_db/models.py
@@ -8,6 +8,7 @@ class Contact(models.Model):
     phone = models.CharField(max_length=30, blank=True, null=True)
     note = models.CharField(max_length=1000, blank=True, null=True)
     color = models.CharField(max_length=150, default='hsl(202, 70%, 85%)')
+    company = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
